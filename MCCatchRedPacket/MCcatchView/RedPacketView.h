db529a74db7ca54b9eb1b782856f0afc8dc66317
zhangzhaopds <zhangzhaopds@foxmail.com>
2017-01-19 09:38:44 +0800

    摄像头捕捉红包

---
+++ MCCatchRedPacket/MCcatchView/RedPacketView.h
@@ -0,0 +1,15 @@
+//
+//  RedPacketView.h
+//  MCscenekit
+//
+//  Created by MC on 16/10/27.
+//  Copyright © 2016年 MC. All rights reserved.
+//
+
+#import "YLImageView.h"
+#import <UIKit/UIKit.h>
+
+@interface RedPacketView : YLImageView
+@property (nonatomic, assign) NSInteger state_size;
+
+@end
